56fc7643ba46408b7df7cb81c4e97bf65e9a18eb
Cvsuhas <suhascv2002@gmail.com>
2023-10-26 23:28:47 +0530

    created class template in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class TodoItems {
+    private:
+    int id;
+    string discription;
+    bool completed;
+
+    TodoItems() {
+        id = 0;
+        discription = "";
+        completed = false;
+    }
+};
 
 int main() {
 
