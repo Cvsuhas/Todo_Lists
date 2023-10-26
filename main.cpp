20f222184de3cbfdfc5d4f6a2e0a8dcfa5dae95d
Cvsuhas <suhascv2002@gmail.com>
2023-10-26 23:40:14 +0530

    added switch conditions and comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <list>
 using namespace std;
 
 class TodoItems {
@@ -16,6 +17,35 @@ class TodoItems {
 };
 
 int main() {
+
+    list<TodoItems> todolists;          //created the list of TodoItems object
+    char input_choice;
+
+    while(1) {
+        system("clear");
+
+        list<TodoItems> :: iterator it;
+        if(todolists.empty()) {         //check if list is empty
+            cout << "Todo Tasks are empty!" << endl;
+        }
+
+        cout << "[a]add Todo Tasks" << endl;
+        cout << "[c]omplete the tasks" << endl;
+        cout << "[q]uit" <<endl;
+        cout << "Enter the choice" << endl;
+        cin >> input_choice;
+
+        switch(input_choice) {          //switch condition for user input
+            case 'a': {
+
+            }
+            case 'b': {
+
+            }
+            case 'q': 
+                exit(0);
+        }
+    }
 
     return 0;
 }
