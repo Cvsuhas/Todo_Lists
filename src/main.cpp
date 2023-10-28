3b008f2fbe6e53efb4c0a2825add4d1f1ab34309
Cvsuhas <suhascv2002@gmail.com>
2023-10-28 14:17:37 +0530

    added swtich conditions

--- src/main.cpp
+++ src/main.cpp
@@ -28,5 +28,28 @@ int main() {
         cout << "[q]uit" <<endl;
         cout << "Enter the choice" << endl;
         cin >> input_choice;
+
+        switch(input_choice) {          //switch condition for user input
+            case 'a': {
+                TodoItems todo;
+                cin >> todo;
+                todolists.push_back(todo);
+            }
+                break;
+            case 'c': {
+                int c_completed;
+                cout<<"Enter the ID to be completed"<<endl;
+                cin>>c_completed;
+                for(it = todolists.begin(); it != todolists.end(); it++) {
+                    if(c_completed == it->GetId()) {
+                        it->SetComplete();
+                        break;
+                    }
+                }
+            }
+                break;
+            case 'q': 
+                exit(0);
+        }
     }
 }
